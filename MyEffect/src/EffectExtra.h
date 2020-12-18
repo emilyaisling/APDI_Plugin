2864090798502e445ebadc3fcfa54598600de3b6
emilyaisling <emilybrown26343@gmail.com>
2020-12-18 15:08:50 +0000

    initial basic chorus

---
+++ MyEffect/src/EffectExtra.h
@@ -0,0 +1,153 @@
+//
+//  EffectExtra.h
+//  Additional Plugin Code
+//
+//  This file is a workspace for developing new DSP objects or functions to use in your plugin.
+//
+//
+//  EffectExtra.h
+//  Additional Plugin Code
+//
+//  This file is a workspace for developing new DSP objects or functions to use in your plugin.
+//
+class MyFilter
+{
+public:
+    void initialise(float coeff)
+    {
+        a = coeff;
+        b = 1 - a;
+    }
+    
+    float process(float input)
+    {
+        return previous = (a * input) + (b * previous);
+    }
+    
+private:
+    float a, b;
+    float previous = 0;
+};
+
+class Osc
+{
+public:
+    Osc()
+    {
+        fPhasePos = 0;
+    }
+    
+    float generate(float fRate, float fDepth)
+    {
+        float fMod = 0;
+        float fPhaseInc = (fTwoPI * fRate) / getSampleRate();
+        fPhasePos += fPhaseInc;
+        if (fPhasePos > fTwoPI)
+        {
+            fPhasePos -= fTwoPI;
+        }
+        
+        if (fPhasePos < M_PI)
+        {
+            fMod = -1 + 2/M_PI * fPhasePos;
+        }
+        else if (fPhasePos >= M_PI)
+        {
+            fMod = 3 - 2/M_PI * fPhasePos;
+        }
+        return fMod * fDepth + fDepth;
+    }
+    
+private:
+    float fPhasePos;
+    const float fTwoPI = 2 * M_PI;
+};
+
+class MyDelay
+{
+public:
+    MyDelay()
+    {
+        iBufferSize = 2 * 192000;
+        
+        pfCircularBuffer = new float[iBufferSize];
+        for (int i = 0; i < iBufferSize; i++)
+        {
+            pfCircularBuffer[i] = 0;
+        }
+        
+        iBufferWritePos = 0;
+        
+        delayTimeFilter.initialise(0.0005);
+    }
+    ~MyDelay()
+    {
+        delete [] pfCircularBuffer;
+    }
+    
+    int TapPos(float fDelTime)
+    {
+        float fBufferReadPos = iBufferWritePos - (delayTimeFilter.process(fDelTime) * getSampleRate());
+        
+        if (fBufferReadPos < 0)
+        {
+            fBufferReadPos += iBufferSize;
+        }
+        
+        return fBufferReadPos;
+    }
+    float InterpolatedRead(float fBufferReadPos)
+    {
+        int iPos1, iPos2;
+        float fPdiff, fVdiff, fResult;
+        
+        iPos1 = (int)fBufferReadPos;
+        iPos2 = iPos1 + 1;
+        if(iPos2 == iBufferSize)
+            iPos2 = 0;
+
+        fPdiff = fBufferReadPos - iPos1;
+        fVdiff = pfCircularBuffer[iPos2] - pfCircularBuffer[iPos1];
+        fResult = pfCircularBuffer[iPos1] + (fVdiff * fPdiff);
+
+        return fResult;
+    }
+    
+    float processSimple(float fIn, float fDelTime, float fDelGain)
+    {
+        pfCircularBuffer[iBufferWritePos] = fIn;
+        iBufferWritePos++;
+        if (iBufferWritePos > iBufferSize - 1)
+        {
+            iBufferWritePos = 0;
+        }
+        
+        return InterpolatedRead(TapPos(fDelTime)) * fDelGain;
+    }
+    
+    float processFeedback(float fFBDelTime, float fFBGain)
+    {
+        int iBufferReadPos;
+        
+        iBufferWritePos++;
+        if(iBufferWritePos == iBufferSize)
+            iBufferWritePos = 0;
+        
+        iBufferReadPos = iBufferWritePos - (delayTimeFilter.process(fFBDelTime) * getSampleRate());
+        if(iBufferReadPos < 0)
+            iBufferReadPos += iBufferSize;
+        
+        return pfCircularBuffer[iBufferReadPos] * fFBGain;
+    }
+    
+    void feedback(float output)
+    {
+        pfCircularBuffer[iBufferWritePos] = output;
+    }
+    
+private:
+    float *pfCircularBuffer;
+    int iBufferSize, iBufferWritePos;
+    MyFilter delayTimeFilter;
+};
+
---
+++ MyEffect/src/EffectPlugin.cpp
@@ -0,0 +1,128 @@
+//
+//  EffectPlugin.cpp
+//  MyEffect Plugin Source Code
+//
+//  Used to define the bodies of functions used by the plugin, as declared in EffectPlugin.h.
+//
+
+#include "EffectPlugin.h"
+
+////////////////////////////////////////////////////////////////////////////
+// EFFECT - represents the whole effect plugin
+////////////////////////////////////////////////////////////////////////////
+
+// Called to create the effect (used to add your effect to the host plugin)
+extern "C" {
+    CREATE_FUNCTION createEffect(float sampleRate) {
+        ::stk::Stk::setSampleRate(sampleRate);
+        
+        //==========================================================================
+        // CONTROLS - Use this array to completely specify your UI
+        // - tells the system what parameters you want, and how they are controlled
+        // - add or remove parameters by adding or removing entries from the list
+        // - each control should have an expressive label / caption
+        // - controls can be of different types: ROTARY, BUTTON, TOGGLE, SLIDER, or MENU (see definitions)
+        // - for rotary and linear sliders, you can set the range of values (make sure the initial value is inside the range)
+        // - for menus, replace the three numeric values with a single array of option strings: e.g. { "one", "two", "three" }
+        // - by default, the controls are laid out in a grid, but you can also move and size them manually
+        //   i.e. replace AUTO_SIZE with Bounds(50,50,100,100) to place a 100x100 control at (50,50)
+        
+        const Parameters CONTROLS = {
+            //  name,       type,              min, max, initial, size
+            {   "Rate",  Parameter::ROTARY, 0.0, 1.0, 0.0, AUTO_SIZE  },
+            {   "Depth",  Parameter::ROTARY, 0.0, 1.0, 0.0, AUTO_SIZE  },
+            {   "Feedback",  Parameter::ROTARY, 0.0, 1.0, 0.0, AUTO_SIZE  }
+        };
+
+        const Presets PRESETS = {
+            { "Preset 1", { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } },
+            { "Preset 2", { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } },
+            { "Preset 3", { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } },
+        };
+
+        return (APDI::Effect*)new MyEffect(CONTROLS, PRESETS);
+    }
+}
+
+// Constructor: called when the effect is first created / loaded
+MyEffect::MyEffect(const Parameters& parameters, const Presets& presets)
+: Effect(parameters, presets)
+{
+    // Initialise member variables, etc.
+}
+
+// Destructor: called when the effect is terminated / unloaded
+MyEffect::~MyEffect()
+{
+    // Put your own additional clean up code here (e.g. free memory)
+}
+
+// EVENT HANDLERS: handle different user input (button presses, preset selection, drop menus)
+
+void MyEffect::presetLoaded(int iPresetNum, const char *sPresetName)
+{
+    // A preset has been loaded, so you could perform setup, such as retrieving parameter values
+    // using getParameter and use them to set state variables in the plugin
+}
+
+void MyEffect::optionChanged(int iOptionMenu, int iItem)
+{
+    // An option menu, with index iOptionMenu, has been changed to the entry, iItem
+}
+
+void MyEffect::buttonPressed(int iButton)
+{
+    // A button, with index iButton, has been pressed
+}
+
+float gainFunc(float parameter)
+{
+    // gain function
+    return parameter;
+}
+
+// Applies audio processing to a buffer of audio
+// (inputBuffer contains the input audio, and processed samples should be stored in outputBuffer)
+void MyEffect::process(const float** inputBuffers, float** outputBuffers, int numSamples)
+{
+    float fIn0, fIn1, fOut0 = 0, fOut1 = 0, fMix = 0;
+    const float *pfInBuffer0 = inputBuffers[0], *pfInBuffer1 = inputBuffers[1];
+    float *pfOutBuffer0 = outputBuffers[0], *pfOutBuffer1 = outputBuffers[1];
+    
+<<<<<<< HEAD
+    float fRate = parameters[0] * 0.09 + 0.01;
+    float fDepth = parameters[1] * 0.025  + 0.025;
+    
+    float fFBGain = parameters[2] * 0.95;
+    fFBGain = 0.7 * fFBGain;
+    
+    float fDelGain = 0.2;
+    float fDelTime = 0;
+    float fDelSig = 0;
+=======
+    float fGain = gainFunc(parameters[0]);
+>>>>>>> d2b601963004ee0ff98fb0e6da3bdd85a78d89af
+    
+    while(numSamples--)
+    {
+        // Get sample from input
+        fIn0 = *pfInBuffer0++;
+        fIn1 = *pfInBuffer1++;
+        
+        // Add your effect processing here
+        fDelTime = osc.generate(fRate, fDepth);
+        
+        fMix = (fIn0 + fIn1) * 0.5f;
+        
+        fDelSig = delay.processFeedback(fDelTime + 0.0005, fFBGain);
+        
+        fOut0 = fMix + fDelSig;
+        fOut1 = fMix + fDelSig;
+        
+        delay.feedback(fOut0);
+        
+        // Copy result to output
+        *pfOutBuffer0++ = fOut0;
+        *pfOutBuffer1++ = fOut0;
+    }
+}
